47f1a656256fc5d2d6b20076fcf8268dba05260c
Gaurav Sharma <gaurav.sharma@punchh.com>
2020-01-30 21:30:45 +0530

    grpc node server.js added

--- grpc-exp/calc/gen_proto.sh
+++ grpc-exp/calc/gen_proto.sh
@@ -3,4 +3,7 @@
 protoc calcpb/calc.proto --go_out=plugins=grpc:.
 
 # npm install -g grpc-tools
+# client
 grpc_tools_node_protoc calcpb/calc.proto --js_out=import_style=commonjs,binary:./client/js --grpc_out=./client/js --plugin=protoc-gen-grpc=`which grpc_tools_node_protoc_plugin` 
+# server
+grpc_tools_node_protoc calcpb/calc.proto --js_out=import_style=commonjs,binary:./server/js --grpc_out=./server/js --plugin=protoc-gen-grpc=`which grpc_tools_node_protoc_plugin` 
---
+++ grpc-exp/calc/server/js/server.js
@@ -0,0 +1,19 @@
+var grpc = require('grpc');
+const Service = require('./calcpb/calc_grpc_pb');
+const messages = require('./calcpb/calc_pb');
+
+function sum(call, callback) {
+	const response = new messages.Response();
+	response.setResult(call.request.getN1() + call.request.getN2());
+	callback(null, response);
+}
+function subtract(call, callback) {
+	const response = new messages.Response();
+	response.setResult(call.request.getN1() - call.request.getN2());
+	callback(null, response);
+}
+
+var server = new grpc.Server();
+server.addService(Service.CalculatorService, { sum, subtract });
+server.bind('0.0.0.0:50050', grpc.ServerCredentials.createInsecure());
+server.start();
